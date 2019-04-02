8d67b4980f8ae803cef429b9c58706862a3477af
joshBremser <joshua.bremser@maine.edu>
2019-04-02 14:57:20 -0400

    can download excel

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@
 
 #Used to make a firefox browser
 from selenium import webdriver 
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary		
 
 #Used to send keypresses to the browser
 from selenium.webdriver.common.keys import Keys 
@@ -10,42 +10,38 @@ import time
 import sys
 import os
 
-UMS_ID = 'username' 
-UMS_PASSWORD = 'password'
+UMS_ID = 'joshua.bremser' 
+UMS_PASSWORD = 
 SPEED_CONSTANT = 1 #Number of seconds to wait for page redirection
 HEADLESS = True
 
-def get_stuff():
-    #Setup
-    binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
-    browser = webdriver.Firefox()
+#Setup
+binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
+browser = webdriver.Firefox()
 
 
-    #UMS Login Page
+#UMS Login Page
 
-    browser.get('https://identity.maine.edu/cas/login' )
+browser.get('https://identity.maine.edu/cas/login' )
 
-    #Acquiring Signin Elements
+#Acquiring Signin Elements
 
-    userid = browser.find_element_by_id('username')
+userid = browser.find_element_by_id('username')
 
-    pwd = browser.find_element_by_id('password')
+pwd = browser.find_element_by_id('password')
 
-    #Signing In
+#Signing In
 
-    userid.send_keys(UMS_ID)
+userid.send_keys(UMS_ID)
 
-    pwd.send_keys(UMS_PASSWORD + Keys.RETURN)
+pwd.send_keys(UMS_PASSWORD + Keys.RETURN)
 
-    #Waiting for page to load
+#Waiting for page to load
 
-    time.sleep(SPEED_CONSTANT)
-    browser.get('https://mycampus.maine.edu/group/usm/home/-/launcher/fire/3937066')
-    time.sleep(SPEED_CONSTANT)
-    browser.get('https://mainestreetcs.maine.edu/psp/CSPRD_1/EMPLOYEE/HRMS/c/RES_LIFE.RL_SS_ROOM_STAFF.GBL?Page=RL_SS_ROOM_DTL&Action=U&CAMPUS=USM&INSTITUTION=UMS06&RL_BLDG_CD=PHILIPPI&RL_FLOOR_CD=1F_1S&RL_QUAD_CD=GORHAM&RL_ROOM_CD=104&STRM=1910')
+time.sleep(SPEED_CONSTANT)
+browser.get('https://mycampus.maine.edu/group/usm/home/-/launcher/fire/3937066')
+time.sleep(SPEED_CONSTANT)
+browser.get('https://mainestreetcs.maine.edu/psp/CSPRD_1/EMPLOYEE/HRMS/c/RES_LIFE.RL_SS_ROOM_STAFF.GBL?Page=RL_SS_ROOM_DTL&Action=U&CAMPUS=USM&INSTITUTION=UMS06&RL_BLDG_CD=PHILIPPI&RL_FLOOR_CD=1F_1S&RL_QUAD_CD=GORHAM&RL_ROOM_CD=104&STRM=1910')
+browser.switch_to.frame('ptifrmtgtframe')
+browser.execute_script("document.getElementById('RL_OCCUPANTS$hexcel$0').click()")
 
-    
-    return something
-
-if __name__ == '__main__':
-    print (get_stuff())
